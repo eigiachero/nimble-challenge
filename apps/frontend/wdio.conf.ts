00f01dc89e0c2787f00ed0ac0924d451e1191fcd
Eze Giachero <eigiachero@gmail.com>
2025-10-27 20:20:35 -0300

    Change wdio log level for ci pipeline

--- apps/frontend/wdio.conf.ts
+++ apps/frontend/wdio.conf.ts
@@ -65,7 +65,7 @@ export const config: WebdriverIO.Config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'warn',
+  logLevel: process.env.CI ? 'error' : 'warn',
   //
   // Set specific log levels per logger
   // loggers:
